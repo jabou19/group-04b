36adfac871ede1d24a4e81d30ada4fc73abe1cd2
Abdirisaq-Ali <91604985+Abdirisaq-Ali@users.noreply.github.com>
2023-08-21 00:55:29 +0200

    Update deploy.sh

--- kubernetes_yaml/deploy.sh
+++ kubernetes_yaml/deploy.sh
@@ -1,5 +1,5 @@
 #echo "${{ secrets.KUBECONFIG }}" | base64 --decode > kubeconfig
 
 
-kubectl --kubeconfig "$KUBECONFIG" apply -f deployment-backend.yaml
-kubectl --kubeconfig "$KUBECONFIG" apply -f deployment-frontend.yaml
+kubectl --kubeconfig kubeconfig apply -f deployment-backend.yaml --server="https://73B3FF2DD54808737DFEAA8AC86F096B.gr7.eu-north-1.eks.amazonaws.com:6443"
+kubectl --kubeconfig kubeconfig apply -f deployment-frontend.yaml --server="https://73B3FF2DD54808737DFEAA8AC86F096B.gr7.eu-north-1.eks.amazonaws.com:6443"
