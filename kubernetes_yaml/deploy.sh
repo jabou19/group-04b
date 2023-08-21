27a62195e5835238c771bd3efead4582bcdba667
Abdullahi Mohamed <aabdi07@student.sdu.dk>
2023-08-21 14:42:51 +0000

    resolved a conflict

--- kubernetes_yaml/deploy.sh
+++ kubernetes_yaml/deploy.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 
 kubectl --kubeconfig "$KUBECONFIG" apply -f deployment-backend.yaml 
@@ -6,10 +5,3 @@ kubectl --kubeconfig "$KUBECONFIG" apply -f deployment-frontend.yaml
 
 kubectl --kubeconfig "$KUBECONFIG" apply -f service-frontend.yaml
 kubectl --kubeconfig "$KUBECONFIG" apply -f service-backend.yaml
-=======
-#echo "${{ secrets.KUBECONFIG }}" | base64 --decode > kubeconfig
-
-
-kubectl --kubeconfig "$KUBECONFIG" apply -f deployment-backend.yaml --server="https://48E6CD27B8AAFFB8876F145214C5758E.gr7.eu-north-1.eks.amazonaws.com:443"
-kubectl --kubeconfig "$KUBECONFIG" apply -f deployment-frontend.yaml --server="https://48E6CD27B8AAFFB8876F145214C5758E.gr7.eu-north-1.eks.amazonaws.com:443"
->>>>>>> 04aa623d46b5201fc54efcb482f2149bfb959ddf
