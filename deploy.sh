a50ff56cbe0968567bf62dd25f1f72d9d59a6669
Abdirisaq-Ali <91604985+Abdirisaq-Ali@users.noreply.github.com>
2023-08-21 10:12:54 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,5 @@ kubectl --token "$KUBECONFIG"
 
 kubectl --kubeconfig "$KUBECONFIG" apply -f deployment-backend.yaml
 kubectl --kubeconfig "$KUBECONFIG" apply -f deployment-frontend.yaml
+
+kubectl --token "$KUBECONFIG"
