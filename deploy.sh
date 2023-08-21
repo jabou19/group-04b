f8f3ceacdb380e641bb03211953c8fc685d413a9
Abdirisaq-Ali <91604985+Abdirisaq-Ali@users.noreply.github.com>
2023-08-21 12:54:15 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,3 @@ kubectl --kubeconfig "$KUBECONFIG" apply -f deployment-frontend.yaml
 kubectl --kubeconfig "$KUBECONFIG" apply -f service-frontend.yaml 
 kubectl --kubeconfig "$KUBECONFIG" apply -f service-backend.yaml 
 
-kubectl config set-credentials cluster-admin --username=admin --password=whatwillhappenhere 
