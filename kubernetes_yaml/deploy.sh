4f6e713d79dcf1805f29ad84ac085045be53b759
Abdirisaq-Ali <91604985+Abdirisaq-Ali@users.noreply.github.com>
2023-08-21 20:08:34 +0200

    Update deploy.sh

--- kubernetes_yaml/deploy.sh
+++ kubernetes_yaml/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-kubectl --kubeconfig "$KUBECONFIG" apply -f deployment-backend.yaml --server https://73B3FF2DD54808737DFEAA8AC86F096B.gr7.eu-north-1.eks.amazonaws.com
-kubectl --kubeconfig "$KUBECONFIG" apply -f deployment-frontend.yaml --server https://73B3FF2DD54808737DFEAA8AC86F096B.gr7.eu-north-1.eks.amazonaws.com
+kubectl --kubeconfig "$KUBECONFIG" apply -f deployment-backend.yaml --server https://73B3FF2DD54808737DFEAA8AC86F096B.gr7.eu-north-1.eks.amazonaws.com:443
+kubectl --kubeconfig "$KUBECONFIG" apply -f deployment-frontend.yaml --server https://73B3FF2DD54808737DFEAA8AC86F096B.gr7.eu-north-1.eks.amazonaws.com:443
 
-kubectl --kubeconfig "$KUBECONFIG" apply -f service-frontend.yaml --server https://73B3FF2DD54808737DFEAA8AC86F096B.gr7.eu-north-1.eks.amazonaws.com
-kubectl --kubeconfig "$KUBECONFIG" apply -f service-backend.yaml --server https://73B3FF2DD54808737DFEAA8AC86F096B.gr7.eu-north-1.eks.amazonaws.com
+kubectl --kubeconfig "$KUBECONFIG" apply -f service-frontend.yaml --server https://73B3FF2DD54808737DFEAA8AC86F096B.gr7.eu-north-1.eks.amazonaws.com:443
+kubectl --kubeconfig "$KUBECONFIG" apply -f service-backend.yaml --server https://73B3FF2DD54808737DFEAA8AC86F096B.gr7.eu-north-1.eks.amazonaws.com:443
