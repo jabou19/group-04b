4d2d11e5f39adcf790e24e8e28267428b5e7a696
Abdirisaq-Ali <91604985+Abdirisaq-Ali@users.noreply.github.com>
2023-08-21 11:46:16 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-kubectl --kubeconfig "secrets.KUBECONFIG" apply -f deployment-backend.yaml --server="https://48E6CD27B8AAFFB8876F145214C5758E.gr7.eu-north-1.eks.amazonaws.com:443" 
-kubectl --kubeconfig "secrets.KUBECONFIG" apply -f deployment-frontend.yaml --server="https://48E6CD27B8AAFFB8876F145214C5758E.gr7.eu-north-1.eks.amazonaws.com:443" 
+kubectl --kubeconfig "$KUBECONFIG" apply -f deployment-backend.yaml --server="https://48E6CD27B8AAFFB8876F145214C5758E.gr7.eu-north-1.eks.amazonaws.com:443" 
+kubectl --kubeconfig "$KUBECONFIG" apply -f deployment-frontend.yaml --server="https://48E6CD27B8AAFFB8876F145214C5758E.gr7.eu-north-1.eks.amazonaws.com:443" 
 
